2827a16703ea01e7c5af60ef02417403a6593c9e
Sunse7 <55135188+Sunse7@users.noreply.github.com>
2023-04-11 14:22:35 +0200

    styling on cards

--- src/Components/EventCard.jsx
+++ src/Components/EventCard.jsx
@@ -1,23 +1,27 @@
-import styles from './EventCard.module.css';
+import styles from "./EventCard.module.css";
 
-function EventCard({upComingEvent}) {
-  const {when, name, where, price} = upComingEvent;
+function EventCard({ upComingEvent }) {
+  const { when, name, where, price } = upComingEvent;
 
   return (
-    <article className={styles.eventContainer}>
-      <section className={styles.date}>
-        <p>{when.date}</p>
-      </section>
-      <section className={styles.infoContainer}>
-        <section className={styles.info}>
-          <h3>{name}</h3>
-          <p>{where}</p>
-          <p>{when.from} - {when.to}</p>
+    <article className={styles.wrapper}>
+      <article className={styles.eventContainer}>
+        <section className={styles.date}>
+          <p>{when.date}</p>
         </section>
-        <section className={styles.price}>
-          <p>{price}</p>
+        <section className={styles.infoContainer}>
+          <section className={styles.info}>
+            <h3>{name}</h3>
+            <p>{where}</p>
+            <p>
+              {when.from} - {when.to}
+            </p>
+          </section>
+          <section className={styles.price}>
+            <p>{price} sek</p>
+          </section>
         </section>
-      </section>
+      </article>
     </article>
   );
 }
